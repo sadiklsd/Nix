35372a5e400ea1878eddf26119947b3c6fcb1438
sadiklsd@gmail.com <sadiklsd@gmail.com>
2014-05-31 11:09:58 +0200

    NEW

---
+++ configuration.nix
@@ -0,0 +1,108 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+imports =
+[ # Include the results of the hardware scan.
+./hardware-configuration.nix
+];
+
+# Use the GRUB 2 boot loader.
+boot.loader.grub.enable = true;
+boot.loader.grub.version = 2;
+# Define on which hard drive you want to install Grub.
+boot.loader.grub.device = "/dev/sda";
+
+networking.hostName = "Sadik"; # Define your hostname.
+# networking.wireless.enable = true; # Enables wireless.
+networking.enableIPv6 = false ;
+networking.firewall.allowedTCPPorts =[ 80 443 22 ];
+networking.firewall.allowPing = true ;
+networking.firewall.enable =true ;
+
+# Select internationalisation properties.
+# i18n = {
+# consoleFont = "lat9w-16";
+# consoleKeyMap = "us";
+# defaultLocale = "en_US.UTF-8";
+# };
+
+
+nixpkgs.config = {
+allowUnfree = true; 
+chromium = {enablePepperFlash = true;};
+
+};
+
+
+
+
+# List packages installed in system profile. To search by name, run:
+# -env -qaP | grep wget
+environment.systemPackages = with pkgs; [
+
+ruby
+wget
+htop
+gitFull
+firefoxWrapper 
+chromiumWrapper
+unrar
+vim
+icedtea7_web 
+busybox
+p7zip
+openvpn
+openjdk
+gparted
+sudo
+dmenu
+zsh
+autoconf
+xorg.xkill
+automake
+nixops
+gcc 
+mercurial 
+bzip2
+python
+lynx
+];
+
+# List services that you want to enable:
+
+# Enable the OpenSSH daemon.
+services.openssh.enable = true;
+services.openssh.permitRootLogin = "yes" ;
+
+# Enable CUPS to print documents.
+# services.printing.enable = true;
+
+# Enable the X11 windowing system.
+services.xserver.enable = true;
+services.xserver.layout = "us";
+services.xserver.xkbOptions = "eurosign:e";
+services.xserver.videoDriver = "virtualbox";
+
+# Enable the Desktop Environment.
+
+services.xserver.displayManager.kdm.enable = true; 
+services.xserver.desktopManager.kde4.enable = true;
+
+services.virtualbox.enable = true;
+
+# Define a user account. Don't forget to set a password with ‘passwd’.
+users.extraUsers.guest = {
+name = "nixxin";
+extraGroups = [ "wheel" ];
+group = "users";
+uid = 1000;
+createHome = true;
+home = "/home/nixxin";
+shell = "/run/current-system/sw/bin/bash";
+};
+
+} 
---
+++ install.sh
@@ -0,0 +1,12 @@
+echo -e "d\nw" |fdisk /dev/sda
+umount -a
+fdisk -l /dev/sda
+echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/sda
+mke2fs -L nixos -t ext4 /dev/sda1
+fdisk -l /dev/sda
+mount /dev/disk/by-label/nixos /mnt
+nixos-generate-config --root /mnt
+
+
+
+echo "nixxin:test" |chpasswd
