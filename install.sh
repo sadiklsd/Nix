1df06b3d89a3975bd6e0141197505474d27c3b23
sadiklsd@gmail.com <sadiklsd@gmail.com>
2014-05-31 13:23:39 +0200

    NEW

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+nix-env -i wget
 echo -e "d\nw" |fdisk /dev/sda
 umount -a
 fdisk -l /dev/sda
@@ -8,14 +9,14 @@ mount /dev/disk/by-label/nixos /mnt
 nixos-generate-config --root /mnt
 cp configuration.nix /mnt/etc/nixos/configuration.nix
 echo "root:toor"|chpasswd
-nixos-install
-echo "nixxin:test"|chpasswd
-cd /
+
+cd /mnt/
 mkdir apps
 cd apps
 git clone https://github.com/elixir-lang/elixir
 git clone https://github.com/elixir-lang/dynamo
-nix-env -i wget
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.0.tar.gz
 tar -xzvf elasticsearch-1.2.0.tar.gz
-alias mix='/apps/bin/mix'
+#alias mix='/apps/bin/mix'
+nixos-install
+echo "nixxin:test"|chpasswd
