70e257b86266d03c52f676edbab698e7b60a843c
sadiklsd@gmail.com <sadiklsd@gmail.com>
2014-06-03 12:54:07 +0200

    NEW

--- install.sh
+++ install.sh
@@ -9,14 +9,5 @@ mount /dev/disk/by-label/nixos /mnt
 nixos-generate-config --root /mnt
 cp configuration.nix /mnt/etc/nixos/configuration.nix
 cp post.sh /mnt/post.sh
-
-#mkdir /mnt/apps
-#cd /mnt/apps
-#pwd
-#git clone https://github.com/elixir-lang/elixir
-#git clone https://github.com/elixir-lang/dynamo
-#tar -xzvf elasticsearch-1.2.0.tar.gz
-#rm -f elasticsearch-1.2.0.tar.gz
-
 nixos-install
-
+reboot
