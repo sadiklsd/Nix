5ba13edb157be9c2c2869474dcf9d1b62547d0ff
sadiklsd@gmail.com <sadiklsd@gmail.com>
2014-06-03 12:22:56 +0200

    NEW

--- install.sh
+++ install.sh
@@ -8,17 +8,15 @@ fdisk -l /dev/sda
 mount /dev/disk/by-label/nixos /mnt
 nixos-generate-config --root /mnt
 cp configuration.nix /mnt/etc/nixos/configuration.nix
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixos.unstable
-nix-channel --update
+cp post.sh /mnt/post.sh
+
 #mkdir /mnt/apps
 #cd /mnt/apps
 #pwd
 #git clone https://github.com/elixir-lang/elixir
 #git clone https://github.com/elixir-lang/dynamo
-#wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.0.tar.gz
 #tar -xzvf elasticsearch-1.2.0.tar.gz
 #rm -f elasticsearch-1.2.0.tar.gz
 
 nixos-install
-useradd -m test
 
