70d2054126aaedc67051900284fd20ff9b248e15
sadiklsd@gmail.com <sadiklsd@gmail.com>
2014-06-05 13:12:29 +0200

    NEW

--- post.sh
+++ post.sh
@@ -5,4 +5,5 @@ nix-channel --update
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.0.tar.gz
 tar -xzvf elasticsearch-1.2.0.tar.gz
 rm -f elasticsearch-1.2.0.tar.gz
-elasticsearch-1.2.0/bin/plugin -install mobz/elasticsearch-head
+elasticsearch-1.2.0/bin/plugin -i mobz/elasticsearch-head
+elasticsearch-1.2.0/bin/plugin -i elasticsearch/marvel/latest
