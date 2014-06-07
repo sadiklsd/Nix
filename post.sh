f82f7aedade3e13f061658dc5f3aeb88db305169
sadik <sadiklsd@gmail.com>
2014-06-07 16:58:56 +0200

    Update post.sh

--- post.sh
+++ post.sh
@@ -2,8 +2,9 @@
 #nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixos.unstable
 #nix-channel --update
 
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.0.tar.gz
-tar -xzvf elasticsearch-1.2.0.tar.gz
-rm -f elasticsearch-1.2.0.tar.gz
-elasticsearch-1.2.0/bin/plugin -i mobz/elasticsearch-head
-elasticsearch-1.2.0/bin/plugin -i elasticsearch/marvel/latest
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.tar.gz
+tar -xzvf elasticsearch-1.2.1.tar.gz
+rm -f elasticsearch-1.2.1.tar.gz
+elasticsearch-1.2.1/bin/plugin -i mobz/elasticsearch-head
+elasticsearch-1.2.1/bin/plugin -i elasticsearch/marvel/latest
+chmod +777 -R elasticsearch-1.2.1/
